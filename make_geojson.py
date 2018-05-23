95138f9a05635d4eb54ab72d5b42b86629ea795f
Michael Wess <michael.wess@boku.ac.at>
2018-05-23 16:19:06 +0200

    increase buffer

--- make_geojson.py
+++ make_geojson.py
@@ -10,7 +10,7 @@ for feature in records:
     #print(feature.shape.__geo_interface__)
     print(feature.record[0])
 
-    geom = shape(feature.shape).buffer(-0.001)
+    geom = shape(feature.shape).buffer(-0.3)
     geom2 = shapely_geojson.Feature(geom, properties={'name': feature.record[0]})
     #print(shapely_geojson.dumps(geom2))
     with open('tiling_grid/geojson/{0}.geojson'.format(feature.record[0]), 'w') as outfile:
