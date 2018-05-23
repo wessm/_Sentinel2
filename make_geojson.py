7cb338b4612d0fc86a842df42381f23979f3aba1
Michael Wess <michael.wess@boku.ac.at>
2018-05-23 15:50:56 +0200

    added buffer

--- make_geojson.py
+++ make_geojson.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import shapefile
+import shapely_geojson
 import geojson
+from shapely.geometry import shape
 
-shape = shapefile.Reader('tiling_grid/shape/sentinel2_tiles_world.shp')
-
-for feature in shape.shapeRecords():
+shapefile = shapefile.Reader('tiling_grid/shape/sentinel2_tiles_world.shp')
+records = shapefile.shapeRecords()
+for feature in records:
     #print(feature.shape.__geo_interface__)
     print(feature.record[0])
+
+    geom = shape(feature.shape).buffer(-0.001)
+    geom2 = shapely_geojson.Feature(geom, properties={'name': feature.record[0]})
+    #print(shapely_geojson.dumps(geom2))
     with open('{0}.geojson'.format(feature.record[0]), 'w') as outfile:
-        geojson.dump(feature.shape.__geo_interface__, outfile)
+        outfile.write(shapely_geojson.dumps(geom2))
